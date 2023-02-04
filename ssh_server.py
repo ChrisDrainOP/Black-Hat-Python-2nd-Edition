644a16de974bb5437e4b2f3921c8b2ea4978d63b
Chris Drain <github.0vo3l@aleeas.com>
2023-02-03 18:41:07 -0800

    updated ssh server file

--- ssh_server.py
+++ ssh_server.py
@@ -17,7 +17,7 @@ class Server (paramiko.ServerInterface):
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def check_auth_password(self, username, password):
-        if (username == '') and (password == ''):
+        if (username == 'username here') and (password == 'password here'):
             return paramiko.AUTH_SUCCESSFUL
 
 if __name__ == '__main__':
