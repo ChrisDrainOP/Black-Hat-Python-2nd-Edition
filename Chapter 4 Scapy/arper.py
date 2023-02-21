734e6bb98bbba51d5c9c22d528b24b13c4b7f73a
Chris Drain <github.0vo3l@aleeas.com>
2023-02-21 03:28:42 -0800

    fixed sndrcv error / replaced with src

--- Chapter 4 Scapy/arper.py
+++ Chapter 4 Scapy/arper.py
@@ -9,8 +9,8 @@ import time
 def get_mac(targetip):
     packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op='who-has', pdst=targetip)
     resp, _ = srp(packet, timeout=2, retry=10, verbose=False)
-    for _,r in resp:
-        return r[Ether].sndrcv
+    for _, r in resp:
+        return r[Ether].src
     return none
 
 class Arper:
